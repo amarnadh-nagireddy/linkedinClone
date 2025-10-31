d75195d26d0f190c94c57494891c1fed823c5e3e
Amarnadh Nagireddy <amarnadh.nagireddy@outlook.com>
2025-11-01 01:09:40 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
+
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.authorization = `Bearer ${token}`;
   }
   return config;
 });
